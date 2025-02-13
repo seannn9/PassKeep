213458e6b8d7506a64a3e14199b08e8193db5d78
seannn9 <naeskirlu9@gmail.com>
2025-02-13 23:21:11 +0800

    separated css for dashboard and app

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Axios from "axios";
-import "../styles/App.css";
+import "../styles/Dashboard.css";
 
 function Dashboard() {
     const [website, setWebsite] = useState("");
