271a820ed359989e10e4e2771d8e4b16cc19727d
seannn9 <naeskirlu9@gmail.com>
2025-02-14 23:35:02 +0800

    added global fontawesome icons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 import "./styles/App.css";
 import Dashboard from "./pages/Dashboard";
 import Welcome from "./pages/Welcome";
 import Login from "./pages/Login";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+library.add(fas);
 
 function App() {
     return (
